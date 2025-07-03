8399700a92a8fd2534b33342c5805115e9c0c24d
Tamiris romano <tamirisr@ufrrj.br>
2025-07-03 19:36:52 -0300

    Começando
    Colocando a estrutura para fazer o código

---
+++ tomate-service/src/main/java/com/trabalhoweb3/tomate_service/TomateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trabalhoweb3.tomate_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomateServiceApplication.class, args);
+	}
+
+}
---
+++ tomate-service/src/test/java/com/trabalhoweb3/tomate_service/TomateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalhoweb3.tomate_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
